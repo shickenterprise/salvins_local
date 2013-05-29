11bff25a5ce6c96edec2f421235ac76967c75741
Gary Shick <gary@fourteeng.com>
2013-05-29 18:29:00 -0400

    Updated Files

--- wp-config.php
+++ wp-config.php
@@ -13,26 +13,15 @@
  *
  * @package WordPress
  */
-
-// ** MySQL settings - You can get this info from your web host ** //
-/** The name of the database for WordPress */
-define('DB_NAME', 'salvins_local');
-
-/** MySQL database username */
-define('DB_USER', 'root');
-
-/** MySQL database password */
-define('DB_PASSWORD', '');
-
-/** MySQL hostname */
-define('DB_HOST', 'localhost');
-
-/** Database Charset to use in creating database tables. */
-define('DB_CHARSET', 'utf8');
-
-/** The Database Collate type. Don't change this if in doubt. */
-define('DB_COLLATE', '');
-
+   if ( file_exists( dirname( __FILE__ ) . '/local-config.php' ) ) {  
+        include( dirname( __FILE__ ) . '/local-config.php' );  
+    }  
+    else {  
+    define('DB_NAME', 'fourtee4_salvins');  
+        define('DB_USER', 'fourtee4');  
+        define('DB_PASSWORD', 'Brown33$');  
+        define('DB_HOST', 'localhost'); 
+    }  
 /**#@+
  * Authentication Unique Keys and Salts.
  *
